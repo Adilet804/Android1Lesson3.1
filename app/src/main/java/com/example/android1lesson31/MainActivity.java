ddc71ee56148619c25321987d38be68f968602f7
Adilet804 <adinormkrutou2@gmail.com>
2021-12-29 19:47:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android1lesson31/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android1lesson31;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GameFragment gameFragment = new GameFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_main, gameFragment);
+        transaction.addToBackStack("GameFragment");
+        transaction.commit();
+    }
+}
